67d75c1a27e9c79a6a6404f9f4a5e14d7f392d97
nkalvi <nkalvi@users.noreply.github.com>
2023-02-08 12:16:35 -0800

    Update path

--- Package.swift
+++ Package.swift
@@ -20,6 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RichEditor",
-            dependencies: ["macColorPicker"])
+            dependencies: ["macColorPicker"],
+            path: ""
+        )
     ]
 )
